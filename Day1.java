8b6a7ed49d46f578a83497dd3d0f329e5b89b57c
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-10 15:16:15 -0800

    Parse input values

--- Day1.java
+++ Day1.java
@@ -3,6 +3,25 @@ import java.util.*;
 import java.math.*;
 
 public class Day1{
+	public static int blocksAway(String[] inputArr){
+		int[] currLoc = {0, 0};
+		System.out.println("(" + currLoc[0] + "," + currLoc[1] + ")");
+		for(String s: inputArr){
+			if(s.startsWith("R")){
+				System.out.println("Moving right " + s.substring(1));
+				
+			}
+			else if (s.startsWith("L")){
+				System.out.println("Moving left " + s.substring(1));
+			}
+			else{
+				System.out.println("Invalid move");
+			}
+		}
+		return 0;
+	}
+
+
 	public static void main(String []args) throws IOException{
 		String tok = "";
 		Scanner reader = new Scanner(new File("input.txt")).useDelimiter(",\\s*");
@@ -15,6 +34,7 @@ public class Day1{
 		reader.close();
 		String[] tempArr = temps.toArray(new String[0]);
 
+		System.out.println(blocksAway(tempArr));
 		// for (String s: tempArr){
 		// 	System.out.println(s);
 		// }
