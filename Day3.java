b9c3d747d0862667ec6c1fe3d55c78e55a049b88
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-12 03:15:42 -0800

    Parse input and convert to int

--- Day3.java
+++ Day3.java
@@ -3,6 +3,22 @@ import java.io.*;
 import java.math.*;
 
 public class Day3{
+	public static int checkTriangle(ArrayList<String> inputArr){
+		int count = 0;
+		int [] intArr = new int[3];
+		for(String s: inputArr){
+			String[] splited = s.split("\\s+");
+			for(int i = 0; i < splited.length; i++){
+				intArr[i] = Integer.parseInt(splited[i]);
+			}
+			if(intArr[0]+intArr[1] < intArr[2] || intArr[1] + intArr[2] < intArr[0] || intArr[0] + intArr[2] < intArr[1]){
+				count++;
+			}
+		}
+
+		return (inputArr.size() - count);
+	}
+
 	public static void main(String[] args) throws IOException{
 		String tok = "";
 		ArrayList<String> temps = new ArrayList<String>();
@@ -14,8 +30,9 @@ public class Day3{
 		}
 		reader.close();
 
-		for(String s : temps){
-			System.out.println(s);
-		}
+		System.out.println(checkTriangle(temps));
+		// for(String s : temps){
+			// System.out.println(s);
+		// }
 	}
 }
