cf0fe3532d61f1fdc2caebb25a9b5d9c8f8b60ac
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-12 20:24:52 -0800

    Finish Day 3

--- Day3.java
+++ Day3.java
@@ -6,17 +6,24 @@ public class Day3{
 	public static int checkTriangle(ArrayList<String> inputArr){
 		int count = 0;
 		int [] intArr = new int[3];
+		int sum1 = 0;
+		int sum2 = 0;
+		int sum3 = 0;
 		for(String s: inputArr){
-			String[] splited = s.split("\\s+");
+			String[] splited = s.trim().split(" +");
 			for(int i = 0; i < splited.length; i++){
 				intArr[i] = Integer.parseInt(splited[i]);
-			}
-			if(intArr[0]+intArr[1] < intArr[2] || intArr[1] + intArr[2] < intArr[0] || intArr[0] + intArr[2] < intArr[1]){
+				// System.out.println(intArr[i]);
+			} 
+			sum1 = intArr[0] + intArr[1];
+			sum2 = intArr[0] + intArr[2];
+			sum3 = intArr[1] + intArr[2];
+			if((sum1 > intArr[2]) && (sum2 > intArr[1]) && (sum3 > intArr[0])){
 				count++;
 			}
 		}
-
-		return (inputArr.size() - count);
+		System.out.println("Total samples:" + inputArr.size());
+		return count;
 	}
 
 	public static void main(String[] args) throws IOException{
@@ -30,7 +37,7 @@ public class Day3{
 		}
 		reader.close();
 
-		System.out.println(checkTriangle(temps));
+		System.out.println("Total possible triangles: " + checkTriangle(temps));
 		// for(String s : temps){
 			// System.out.println(s);
 		// }
