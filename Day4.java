c2e7c34221ae18100a2f5049ea506e9db20a65df
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-14 02:48:32 -0800

    Find frequency of letters and compare to checksum. Partially working...

--- Day4.java
+++ Day4.java
@@ -2,22 +2,49 @@ import java.util.*;
 import java.math.*;
 import java.io.*;
 import java.util.regex.*;
+import java.util.stream.Collectors;
 
 public class Day4{
-	public static int realRooms(ArrayList<String> inputArr){
+	public static int realRooms(List<String> inputArr){
 		int sum = 0;
-		ArrayList<String> list = new ArrayList<String>();
+		List<String> list = new ArrayList<String>();
 		for(String s: inputArr){
+			Map<Character, Integer> freq = new HashMap<Character, Integer>();
+			for(int i = 0; i < s.length()-1; i++){
+				s.chars().filter(x -> x != '-').forEach(x -> freq.put((char) x, freq.getOrDefault((char)x, 0)+1));
+			}
+
+			String top5 = freq.entrySet().stream()
+				.sorted(Map.Entry.comparingByKey())
+				.sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+				.limit(5)
+				.map(Map.Entry::getKey)
+				.map(x -> Character.toString(x))
+				.collect(Collectors.joining(""));
+
+			//Splits orignal string input by dashes
 			list = new ArrayList<String>(Arrays.asList(s.split("-")));
 
+			//Splits number in brackets and appends to end of list
      		Matcher m = Pattern.compile("\\[([^)\\]]+)").matcher(s);
      		int pos = -1;
 			while(m.find(pos+1)){
 				pos = m.start();
 				list.add(m.group(1));
 			}
+			
+			//Replace string where bracket number was
 			String str = list.get(list.size()-2);
 			list.set(list.size()-2, str.substring(0, str.indexOf("[")));
+
+			//check if match checksum
+			String checkSum = list.get(list.size()-1);
+			System.out.print("Checksum: " + checkSum + "  Top 5: " + top5);
+			if(top5.equals(checkSum)){
+				System.out.print("-----MATCH");
+				sum += Integer.parseInt(list.get(list.size()-2));
+			}
+			System.out.println("");
 		}
 		return sum;
 	}
@@ -25,7 +52,7 @@ public class Day4{
 	public static void main(String[] args) throws IOException{
 		String tok = "";
 		Scanner reader = new Scanner(new File("input4.txt"));
-		ArrayList<String> temps = new ArrayList<String>();
+		List<String> temps = new ArrayList<String>();
 
 		while(reader.hasNext()){
 			tok = reader.nextLine();
