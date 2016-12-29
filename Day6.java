7f97103ba84c44394910bf0fa2c4e1acebd59460
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-29 03:49:03 -0800

    Made hasmaps to hold data and counts

--- Day6.java
+++ Day6.java
@@ -2,7 +2,29 @@ import java.util.*;
 import java.math.*;
 import java.io.*;
 
-public class Day5{
+public class Day6{
+	public static void unjamComm(List<String> data){
+		HashMap<Character, Integer> frequency1 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> frequency2 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> frequency3 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> frequency4 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> frequency5 = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> frequency6 = new HashMap<Character, Integer>();
+		for(String s : data){
+			for(int i = 0; i < s.length(); i++){
+				if(i == 1){
+					Integer count = frequency1.get(s.charAt(i));
+					frequency1.put(s.charAt(i), count++);
+				}
+				else if(i == 2){
+					Integer count = frequency2.get(s.charAt(i));
+					frequency2.put(s.charAt(i), count++);
+				}
+			}	
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception{
 		String tok = "";
 		List<String> temps = new ArrayList<String>();
@@ -13,5 +35,6 @@ public class Day5{
 			temps.add(tok);
 		}
 		reader.close();
+		unjamComm(temps);
 	}
 }
