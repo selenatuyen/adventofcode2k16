8bfa3fbaf8fc007754ffb762634f6b45044dd8c0
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-30 02:50:10 -0800

    Finish day 6

--- Day6.java
+++ Day6.java
@@ -1,40 +1,56 @@
 import java.util.*;
-import java.math.*;
+import java.util.Collections;
 import java.io.*;
 
 public class Day6{
-	public static void unjamComm(List<String> data){
-		HashMap<Character, Integer> frequency1 = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> frequency2 = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> frequency3 = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> frequency4 = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> frequency5 = new HashMap<Character, Integer>();
-		HashMap<Character, Integer> frequency6 = new HashMap<Character, Integer>();
-		for(String s : data){
-			for(int i = 0; i < s.length(); i++){
-				if(i == 1){
-					Integer count = frequency1.get(s.charAt(i));
-					frequency1.put(s.charAt(i), count++);
+	public static String[] getMessage(List<String> input){
+		char[] message = new char[8];
+		char[] message2 = new char[8];
+		for(int i = 0; i < input.get(0).length(); i++){
+			int count = 0;
+			Map<Character, Integer> freq = new HashMap<Character, Integer>();
+			for(String s : input){
+				char c = s.charAt(i);
+				if(freq.containsKey(c)) {
+					count = freq.get(c)+1;
+					freq.put(c, count);
 				}
-				else if(i == 2){
-					Integer count = frequency2.get(s.charAt(i));
-					frequency2.put(s.charAt(i), count++);
+				else{
+					count = 1;
+					freq.put(c, count);
 				}
-			}	
-		}
+			}
 
+		char mostCommon = 'a';
+		char leastCommon = 'a';
+		for(Map.Entry<Character, Integer> entry : freq.entrySet()){
+			if(entry.getValue() > freq.get(mostCommon)){
+				mostCommon = entry.getKey();
+			}
+			if(entry.getValue() < freq.get(leastCommon)){
+				leastCommon = entry.getKey();
+			}
+		}
+		message[i] = mostCommon;
+		message2[i] = leastCommon;			
+		}
+		String m1 = new String(message);
+		String m2 = new String(message2);
+		String[] messages = {m1, m2};
+		return messages;
 	}
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException {
 		String tok = "";
-		List<String> temps = new ArrayList<String>();
-
 		Scanner reader = new Scanner(new File("input6.txt"));
+		List<String> lines = new ArrayList<String>();
+
 		while(reader.hasNext()){
 			tok = reader.nextLine();
-			temps.add(tok);
+			lines.add(tok);
 		}
-		reader.close();
-		unjamComm(temps);
+		String [] answer = getMessage(lines);
+	    System.out.println("most " + answer[0]);
+	    System.out.println("least " + answer[1]);
 	}
 }
