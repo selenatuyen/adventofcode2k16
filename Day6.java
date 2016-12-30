b58a07ba021739b9aee68d231c12414151006a74
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-30 03:02:40 -0800

    Finish day 6 update with comments

--- Day6.java
+++ Day6.java
@@ -5,35 +5,35 @@ public class Day6{
 	public static String[] getMessage(List<String> input){
 		char[] message = new char[8];
 		char[] message2 = new char[8];
-		for(int i = 0; i < input.get(0).length(); i++){
+		for(int i = 0; i < input.get(0).length(); i++){ //for length of input string (8 chars a line)
 			int count = 0;
-			Map<Character, Integer> freq = new HashMap<Character, Integer>();
-			for(String s : input){
-				char c = s.charAt(i);
-				if(freq.containsKey(c)) {
-					count = freq.get(c)+1;
-					freq.put(c, count);
+			Map<Character, Integer> freq = new HashMap<Character, Integer>(); //map to hold char and freq
+			for(String s : input){  //for each string in input
+				char c = s.charAt(i);  //get character in ith position
+				if(freq.containsKey(c)) {  //if character is already in map
+					count = freq.get(c)+1; //increase count
+					freq.put(c, count);    //update hash
 				}
 				else{
-					count = 1;
+					count = 1;			//char is not in map
 					freq.put(c, count);
 				}
 			}
 
-		char mostCommon = 'a';
-		char leastCommon = 'a';
-		for(Map.Entry<Character, Integer> entry : freq.entrySet()){
-			if(entry.getValue() > freq.get(mostCommon)){
-				mostCommon = entry.getKey();
+		char mostCommon = 'a';		//init comparator arbritrary
+		char leastCommon = 'a';		//init comparator arbritrary
+		for(Map.Entry<Character, Integer> entry : freq.entrySet()){ //for each entry in freq
+			if(entry.getValue() > freq.get(mostCommon)){ //if current entry's count is larger than mostCommon's count
+				mostCommon = entry.getKey();	//set as most common
 			}
-			if(entry.getValue() < freq.get(leastCommon)){
-				leastCommon = entry.getKey();
+			if(entry.getValue() < freq.get(leastCommon)){ //if current entry's count is less than least common's count
+				leastCommon = entry.getKey();	//set as least common
 			}
 		}
-		message[i] = mostCommon;
-		message2[i] = leastCommon;			
+		message[i] = mostCommon;	//update letter for the ith position
+		message2[i] = leastCommon;	//update letter for the ith positon
 		}
-		String m1 = new String(message);
+		String m1 = new String(message); 
 		String m2 = new String(message2);
 		String[] messages = {m1, m2};
 		return messages;
