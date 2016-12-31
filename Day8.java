61c3c44ca7413e3c0f03c46fae7873da03189623
Selena Tuyen <sltuyen@ucdavis.edu>
2016-12-31 04:05:57 -0800

    Day 8: display screen

--- Day8.java
+++ Day8.java
@@ -11,7 +11,31 @@ public class Day8{
 	}
 
 	public static int pixelCount(List<String> input){
+		int count = 0;
+		int x = 7;
+		int y = 3;
+		char[][] screen = new char[x][y];
+		for(int i = 0; i < x; i++){
+			for(int j = 0; j < y; j++){
+				screen[i][j] = '.';
+			}
+		}
+		displayScreen(screen, x, y);
+		return count;
+	}
 
+	public static void displayScreen(char[][] screen, int x, int y){
+		for(int i = 0; i < y; i++){
+			for(int j = 0; j < x; j++){
+				if(screen[j][i] == '.'){
+					System.out.print(screen[j][i]);
+				}
+				else if(screen[j][i] == '#'){
+					System.out.print(screen[j][i]);
+				}
+			}
+			System.out.println("");
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
